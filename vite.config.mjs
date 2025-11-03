27f4163fd7eba133e95522700db8da150d6c512f
acwinters01 <acwinters03@gmail.com>
2025-11-03 10:22:05 -0600

    fix: correct PlaylistHandling import path

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/jammming/',
     plugins: [react()],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
     test: {
         globals: true,
         environment: 'jsdom',
